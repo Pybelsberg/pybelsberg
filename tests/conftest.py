d7d097a1f2e4c21c47002cadc3be4ed8e1bab790
Robert Lehmann <mail@robertlehmann.de>
2014-08-01 20:57:40 +0200

    Use ordinary class for Point and give it a repr.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,18 @@ import pytest
 
 @pytest.fixture
 def Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def distance(self, other):
-        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
 
-    return type('Point', (), dict(__init__=__init__, distance=distance))
+        def distance(self, other):
+            return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+
+        def __repr__(self):
+            return "Point(%r, %r)" % (self.x, self.y)
+
+    return Point
 
 
 @pytest.fixture
